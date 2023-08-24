4c3e34577bf0684d3883826f8105bf5f6f8a15bd
RavichandraYellenki <134284687+RavichandraYellenki@users.noreply.github.com>
2023-08-24 13:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function removeColor(){
+	let selectElement = document.getElementById("colorSelect");
+	let output = selectElement.value;
+	if(output ==="red"){
+		let elementToBeRemoved = document.getElementById("1");
+		elementToBeRemoved.remove();
+	}
+	else if(output ==="green"){
+		let elementToBeRemoved = document.getElementById("2");
+		elementToBeRemoved.remove();
+	}
+	else if(output ==="white"){
+		let elementToBeRemoved = document.getElementById("3");
+		elementToBeRemoved.remove();
+	}
+	else if(output ==="black"){
+		let elementToBeRemoved = document.getElementById("4");
+		elementToBeRemoved.remove();
+	}
+}
